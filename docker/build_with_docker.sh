c49ba1f9d46d9c71f428f16a59c5aee3e9da722f
谢东平 (XIE Dongping) <dongping.xie.tud@gmail.com>
2015-10-11 21:12:29 +0200

    Building environmental variables

--- docker/build_with_docker.sh
+++ docker/build_with_docker.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 image_name="xiedongping/book-mbe"
-echo $S3FLAGS
 echo $image_name
 
 sudo docker build -t $image_name MBE
-sudo docker run -e "S3FLAGS=$S3FLAGS" -t $image_name
+sudo docker run -t -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" -e "AWS_SECRET_KEY=$AWS_SECRET_KEY" $image_name
